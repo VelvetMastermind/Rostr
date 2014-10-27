71fff42b1dbfcb55327d77649c79efbde1faae6d
joeythomaschaske <joeythomaschaske@gmail.com>
2014-10-26 23:19:44 -0500

    Updated Scraper
    Scraper is succesfully navigating courses, but still needs to be
    updated to parse useful information

--- src/com/velvetmastermind/rostr/UpdateCoursesServlet.java
+++ src/com/velvetmastermind/rostr/UpdateCoursesServlet.java
@@ -3,6 +3,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
+import org.jsoup.examples.*;
+import org.jsoup.nodes.Node;
 
 import java.io.IOException;
 import java.util.List;
@@ -24,7 +26,16 @@ public class UpdateCoursesServlet extends HttpServlet
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
-		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-		Elements newsHeadlines = doc.select("#mp-itn b a");
+		Document doc = Jsoup.connect("http://www4.uwm.edu/schedule/index.cfm?a1=subject_details&subject=COMPSCI&strm=2149").get();
+		Elements td = doc.getElementsByTag("table");
+		Elements useful = new Elements();
+		for(Element element : td){
+			String x = element.toString();
+			if(x.contains("COMPSCI")){
+				useful.add(element);
+			}
+		}
+		useful.remove(0);
+		td.clear();
 	}
 }
