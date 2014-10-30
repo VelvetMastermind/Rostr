beaa1d20b915bc6f690a407fb1e4dbb271000997
Andrew Budziszek <abudziszek@gmail.com>
2014-10-29 20:51:31 -0500

    Login Servlet Cleanup
    Removed unused import.

--- src/com/velvetmastermind/rostr/LoginServlet.java
+++ src/com/velvetmastermind/rostr/LoginServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.DatastoreService;
-
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet
 {
