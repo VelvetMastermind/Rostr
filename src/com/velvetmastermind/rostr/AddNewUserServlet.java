920ccdddf3534e454c09307be8123539152551d9
Andrew Budziszek <abudziszek.dev@gmail.com>
2014-11-23 14:47:33 -0600

    Workspace changes | Add New user Servlet Change
    Add New User Servlet Change

--- src/com/velvetmastermind/rostr/AddNewUserServlet.java
+++ src/com/velvetmastermind/rostr/AddNewUserServlet.java
@@ -1,6 +1,7 @@
 package com.velvetmastermind.rostr;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.Entity;
+import com.sun.org.apache.xalan.internal.xsltc.dom.ArrayNodeListIterator;
 
 @SuppressWarnings("serial")
 public class AddNewUserServlet extends HttpServlet
@@ -23,17 +25,31 @@ public class AddNewUserServlet extends HttpServlet
     {
     	try
     	{
-	        String username = req.getParameter("fullName");
-	        String password = req.getParameter("newPassword");
-	        String pantherID = req.getParameter("pantherID");
-	        String roomNumber = req.getParameter("roomNumber"); 
-	        String phoneNumber = req.getParameter("phoneNumber");
-	        String officeHours = req.getParameter("officeHours");
+			ArrayList<String> errors = new ArrayList<>();
+			String username = "";
+			String password = "";
+			String pantherID = "";
+			String roomNumber = "";
+			String phoneNumber = "";
+			String officeHours = "";
+	        username = req.getParameter("fullName");
+	        password = req.getParameter("newPassword");
+	        pantherID = req.getParameter("pantherID");
+	        roomNumber = req.getParameter("roomNumber");
+	        phoneNumber = req.getParameter("phoneNumber");
+	        officeHours = req.getParameter("officeHours");
 	        int iAccessLevel = -1;
 	        boolean success = false;
+
+			if(username.length() == 0)
+				errors.add("Username is invalid!");
+			if(password.length() < 6 || password.length() > 20)
+				errors.add("Invalid password! (Password must be 6-20 characters long)");
+			if(pantherID.length() == 0)
+				errors.add("pantherID is invalid!");
 	
 	        DatastoreService ds = rostrUtilities.getDatastore();
-	        Entity e = rostrUtilities.createEntity("user"); 
+	        Entity e = rostrUtilities.createEntity("user");
 
 	        success = rostrUtilities.addUserToDatastore(e, username, password, pantherID, roomNumber, phoneNumber, officeHours, iAccessLevel, ds);
 	        if(success)
